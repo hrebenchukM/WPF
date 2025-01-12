a153476ea2ee8ce07f1bc71a147caef8950c4163
Mary <marya101204@gmail.com>
2025-01-12 02:31:55 +0200

    ..

--- Hw4Pocket-book/MainWindow.xaml.cs
+++ Hw4Pocket-book/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,46 +27,158 @@ namespace Hw4Pocket_book
         }
 
 
-        private void Open_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            BookRecords bookrecords = Resources["bookrecords"] as BookRecords; ;//обращаемся к ресурсам окна и получаем обьект класса ProgrammingLanguage
+            Record lang = new Record();
+            lang.Name = bookrecords.RecordName;
+            lang.Adress = bookrecords.RecordAdress;
+            lang.Phone = bookrecords.RecordPhone;
+            bookrecords.Records.Add(lang);
+        }
 
-            if (openFileDialog1.ShowDialog() == true)
+        private void listBox1_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            try
             {
-                
+                BookRecords bookrecords = Resources["bookrecords"] as BookRecords; ;//обращаемся к ресурсам окна и получаем обьект класса ProgrammingLanguage
+                if (bookrecords.Index_selected_listbox == -1)
+                    return;
+                Record lang = bookrecords.Records[bookrecords.Index_selected_listbox];
+                bookrecords.RecordName = lang.Name;
+                bookrecords.RecordAdress = lang.Adress;
+                bookrecords.RecordPhone = lang.Phone;
+            }
+            catch { }
+        }
+
+        private void langName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textbox = sender as TextBox;
+            textbox.Text = string.Empty;
+        }
+    }
+
+
+
+
+
+
+    public class Record : INotifyPropertyChanged
+    {
+        private string name;
+        private string adress;
+        private string phone;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+        public string Adress
+        {
+            get
+            {
+                return adress;
+            }
+            set
+            {
+                adress = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(Adress)));
+            }
+        }
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                phone = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(Phone)));
             }
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
 
-
-        private void SaveAs_Click(object sender, RoutedEventArgs e)
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
         {
-            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            PropertyChanged?.Invoke(this, e);
+        }
+    }
 
-            if (saveFileDialog1.ShowDialog() == true)
+    public class BookRecords : INotifyPropertyChanged
+    {
+        //коллекция хранит не строки а обьекты Language
+        public ObservableCollection<Record> Records { get; set; } = new ObservableCollection<Record>();//как Лист ,но реализует еще паттерн наблюдатель,то есть при изменениях коллекции,то она уведомляет сообщением наблюдателя о том что с ней происходило . На эту коллекцию подписан ListBox ItemsSource="{Binding Languages}"  
+
+        private int index_selected_listbox = -1;
+
+        public int Index_selected_listbox
+        {
+            get { return index_selected_listbox; }
+            set
             {
-                
-                MessageBox.Show("Файл сохранен!");
+                index_selected_listbox = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(Index_selected_listbox)));//в метод передали данные о том какое сейчас свойство устанавливается ,чтоб понимать какого подписчика дергать нам        
             }
         }
 
+        private string name;
+        private string adress;
+        private string phone;
 
-
-        private void Add_Click(object sender, RoutedEventArgs e)
+        public string RecordName
         {
-
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(RecordName)));//в метод передали данные о том какое сейчас свойство устанавливается ,чтоб понимать какого подписчика дергать нам        
+            }
+        }
+        public string RecordAdress
+        {
+            get
+            {
+                return adress;
+            }
+            set
+            {
+                adress = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(RecordAdress)));//в метод передали данные о том какое сейчас свойство устанавливается ,чтоб понимать какого подписчика дергать нам        
+            }
+        }
+        public string RecordPhone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                phone = value;
+                OnPropertyChanged(new PropertyChangedEventArgs(nameof(RecordPhone)));//в метод передали данные о том какое сейчас свойство устанавливается ,чтоб понимать какого подписчика дергать нам        
+            }
         }
 
-        private void Edit_Click(object sender, RoutedEventArgs e)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
         {
-          
+            PropertyChanged?.Invoke(this, e);//оповещение конкретного подписчика чтоб не дергать всех подряд .                    
+            //подписчик-это тот элемент который привязывался  Text="{Binding Name}
         }
-
-        private void Remove_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-
     }
 }
